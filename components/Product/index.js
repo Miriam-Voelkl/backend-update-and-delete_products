9945da272d19d69d8dd6f8e6c107af1f6c79f674
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-26 12:54:32 +0100

    add edit and delete products

--- components/Product/index.js
+++ components/Product/index.js
@@ -3,12 +3,42 @@ import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
 import Comments from "../Comments";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
+  const [isEditMode, setIsEditMode] = useState(false);
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct(id) {
+    const response = await fetch(`/api/products/${id}`, { method: "DELETE" });
+    if (response.ok) {
+      await response.json();
+      router.push("/");
+      return;
+    } else {
+      console.log(response.status);
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -25,6 +55,26 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Edit product
+      </button>
+      <button type="button" onClick={() => handleDeleteProduct(id)}>
+        Delete fish
+      </button>
+      {isEditMode && (
+        <ProductForm
+          onSubmit={handleEditProduct}
+          isEditMode={isEditMode}
+          name={data.name}
+          description={data.description}
+          price={data.price}
+        />
+      )}
       {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
